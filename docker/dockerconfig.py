2b35934644289cee123dbe62bfe23f1eaeafde8e
Malte Krupa <github@nafn.de>
2019-05-26 14:05:10 +0200

    Add dockerfiles, build-container script, custom docker configuration for privacyscanner and a run script to run different version of local and remote versions inside a container

---
+++ docker/dockerconfig.py
@@ -0,0 +1,20 @@
+QUEUE_DB_DSN = 'dbname=privacyscore user=privacyscore password=privacyscore host=localhost'
+MAX_EXECUTION_TIMES = {None: 300}
+SCAN_MODULE_OPTIONS = {
+        'chromedevtools': {
+                'chrome_remote_host': '172.20.0.2'
+                }
+        }
+SCAN_MODULES = [
+                'privacyscanner.scanmodules.chromedevtools.ChromeDevtoolsScanModule',
+                'privacyscanner.scanmodules.dns.DNSScanModule',
+                'privacyscanner.scanmodules.mail.MailScanModule',
+                'privacyscanner.scanmodules.serverleaks.ServerleaksScanModule',
+                'privacyscanner.scanmodules.testsslsh.TestsslshHttpsScanModule',
+                'privacyscanner.scanmodules.testsslsh.TestsslshMailScanModule'
+        ]
+NUM_WORKERS = 0
+MAX_EXECUTIONS = 100
+RAVEN_DSN = None
+MAX_TRIES = 3
+STORAGE_PATH = '~/.local/share/privacyscanner'
